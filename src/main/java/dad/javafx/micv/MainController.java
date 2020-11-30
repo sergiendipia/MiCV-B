5e490d2b0dbc1673c97fe6618c5b67470d0a2602
imgenki <s.rguez00@gmail.com>
2020-11-30 15:37:38 +0000

    upd logica main

--- src/main/java/dad/javafx/micv/MainController.java
+++ src/main/java/dad/javafx/micv/MainController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import org.apache.commons.io.FileUtils;
@@ -18,6 +19,7 @@ import dad.javafx.micv.formacion.FormacionController;
 import dad.javafx.micv.model.CV;
 import dad.javafx.micv.personal.PersonalController;
 import dad.javafx.micv.utils.JSONUtils;
+import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
@@ -25,50 +27,54 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 public class MainController implements Initializable {
-	
+
+	String ruta;
 	// controllers
-	
+
 	private PersonalController personalController = new PersonalController();
 	private ContactoController contactoController = new ContactoController();
 	private FormacionController formacionController = new FormacionController();
 	private ExperienciaController experienciaController = new ExperienciaController();
 	private ConocimientosController conocimientosController = new ConocimientosController();
 	// model
-	
+
 	private ObjectProperty<CV> cv = new SimpleObjectProperty<>();
-	
+
 	// view
 
-    @FXML
-    private BorderPane view;
+	@FXML
+	private BorderPane view;
 
-    @FXML
-    private Tab personalTab;
+	@FXML
+	private Tab personalTab;
 
-    @FXML
-    private Tab contactoTab;
+	@FXML
+	private Tab contactoTab;
 
-    @FXML
-    private Tab formacionTab;
+	@FXML
+	private Tab formacionTab;
 
-    @FXML
-    private Tab experienciaTab;
+	@FXML
+	private Tab experienciaTab;
 
-    @FXML
-    private Tab conocimientosTab;
-	
+	@FXML
+	private Tab conocimientosTab;
+
 	public MainController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
 		loader.setController(this);
 		loader.load();
 	}
-	
+
 	public BorderPane getView() {
 		return view;
 	}
@@ -82,95 +88,120 @@ public class MainController implements Initializable {
 		experienciaTab.setContent(experienciaController.getView());
 		conocimientosTab.setContent(conocimientosController.getView());
 		cv.addListener((o, ov, nv) -> onCVChanged(o, ov, nv));
-		
+
 		cv.set(new CV());
-		
+
 	}
 
-    private void onCVChanged(ObservableValue<? extends CV> o, CV ov, CV nv) {
-    	
-    	if (ov != null) {
-    		
-    		personalController.personalProperty().unbind(); // desbindeo personalProperty del CV anterior
-    		contactoController.contactoProperty().unbind();
-    		formacionController.formacionProperty().unbind();
-    		experienciaController.experienciasProperty().unbind();
-    		conocimientosController.habilidadesProperty().unbind();
-    		
- 
-    		
-    	}
+	private void onCVChanged(ObservableValue<? extends CV> o, CV ov, CV nv) {
 
-    	if (nv != null) {
-    		
-    		personalController.personalProperty().bind(nv.personalProperty()); // bindeo personalProperty del nuevo CV
-    		contactoController.contactoProperty().bind(nv.contactoProperty());
-    		formacionController.formacionProperty().bind(nv.formacionProperty());
-    		experienciaController.experienciasProperty().bind(nv.experienciasProperty());
-    		conocimientosController.habilidadesProperty().bind(nv.habilidadesProperty());
+		if (ov != null) {
 
-    		
-    	}
-    	
+			personalController.personalProperty().unbind(); // desbindeo personalProperty del CV anterior
+			contactoController.contactoProperty().unbind();
+			formacionController.formacionProperty().unbind();
+			experienciaController.experienciasProperty().unbind();
+			conocimientosController.habilidadesProperty().unbind();
+
+		}
+
+		if (nv != null) {
+
+			personalController.personalProperty().bind(nv.personalProperty()); // bindeo personalProperty del nuevo CV
+			contactoController.contactoProperty().bind(nv.contactoProperty());
+			formacionController.formacionProperty().bind(nv.formacionProperty());
+			experienciaController.experienciasProperty().bind(nv.experienciasProperty());
+			conocimientosController.habilidadesProperty().bind(nv.habilidadesProperty());
+
+		}
+
 	}
 
 	@FXML
-    void onAbrirAction(ActionEvent event) {
+	void onAbrirAction(ActionEvent event) {
 
-    	FileChooser fileChooser = new FileChooser();
-    	fileChooser.setTitle("Abrir un currículum");
-    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Currículum Vitae (*.cv)", "*.cv"));
-    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Todos los archivos", "*.*"));
-    	File cvFile = fileChooser.showOpenDialog(App.getPrimaryStage());
-    	if (cvFile != null) {
-    		try {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Abrir un currículum");
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("Currículum Vitae (*.cv)", "*.cv"));
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("Todos los archivos", "*.*"));
+		File cvFile = fileChooser.showOpenDialog(App.getPrimaryStage());
+		if (cvFile != null) {
+			try {
 				cv.set(JSONUtils.fromJson(cvFile, CV.class));
+				ruta = cvFile.getAbsolutePath();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-    	}
-		
-    }
+		}
 
-    @FXML
-    void onAcercaDeAction(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void onAcercaDeAction(ActionEvent event) {
 
-    @FXML
-    void onGuardarAction(ActionEvent event) {
+	}
 
-    }
-
-    @FXML
-    void onGuardarComoAction(ActionEvent event) {
-
-    	FileChooser fileChooser = new FileChooser();
-    	fileChooser.setTitle("Guardar un currículum");
-    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Currículum Vitae (*.cv)", "*.cv"));
-    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Todos los archivos", "*.*"));
-    	File cvFile = fileChooser.showSaveDialog(App.getPrimaryStage());
-    	if (cvFile != null) {
-    		
-    		try {
+	@FXML
+	void onGuardarAction(ActionEvent event) {
+		/*
+		 * Necesito comprobar que se ha abierto un archivo Y sobreescribir el archivo al
+		 * pulsar guardar
+		 */
+		if (ruta != null) {
+			File cvFile = new File(ruta);
+			try {
 				JSONUtils.toJson(cvFile, cv.get());
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    		
-    	}
-    	
-    }
+		} else {
+			onGuardarComoAction(event);
+		}
 
-    @FXML
-    void onNuevoAction(ActionEvent event) {
-    	System.out.println("Has pulsado nuevo");
-    	cv.set(new CV());
-    }
+	}
 
-    @FXML
-    void onSalirAction(ActionEvent event) {
+	@FXML
+	void onGuardarComoAction(ActionEvent event) {
 
-    }
-	
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Guardar un currículum");
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("Currículum Vitae (*.cv)", "*.cv"));
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("Todos los archivos", "*.*"));
+		File cvFile = fileChooser.showSaveDialog(App.getPrimaryStage());
+		if (cvFile != null) {
+
+			try {
+				JSONUtils.toJson(cvFile, cv.get());
+				ruta = cvFile.getAbsolutePath();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	@FXML
+	void onNuevoAction(ActionEvent event) {
+		System.out.println("Has pulsado nuevo");
+		cv.set(new CV());
+		ruta = null;
+	}
+
+	@FXML
+	void onSalirAction(ActionEvent event) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Dialogo de confirmación");
+		alert.setHeaderText("Vas a cerrar el programa");
+		alert.setContentText("¿Estás seguro?");
+
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK) {
+			Platform.exit();
+		} else {
+			// ... user chose CANCEL or closed the dialog
+		}
+	}
+
 }
